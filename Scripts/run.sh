2bd90ca7fc57039f41926295aa1f4454942b5e52
Giang Truong Le <30622735+ginglove@users.noreply.github.com>
2020-12-28 13:30:35 +0700

    Update run.sh

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -12,6 +12,6 @@ echo "$TIME"
 echo "$K6_STAGES"
 ############ RUNNING TEST ##################
 echo "=== Run K6 performance test==="
-#TESTCASE_DEMO="$PWD/Test_Cases/Demo_test.js"
-TESTCASE_DEMO="../Test_Cases/Demo_test.js"
+TESTCASE_DEMO="$PWD/Test_Cases/Demo_test.js"
+#TESTCASE_DEMO="../Test_Cases/Demo_test.js"
 k6 run --summary-trend-stats "min,avg,max,med,p(75),p(90),p(95),p(99),p(99.99)" -s "$STAGE_1:$USER_1" -s "$STAGE_2:$USER_2" -s "$STAGE_3:$USER_3" --summary-export ./result.csv $TESTCASE_DEMO
